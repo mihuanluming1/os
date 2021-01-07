e9de9724797b3846b48a3d617e884d7e85a5ecac
cyyyii <76993189+cyyyii@users.noreply.github.com>
2021-01-07 17:12:26 +0800

    Add files via upload

---
+++ src/file_system/inode.java
@@ -0,0 +1,25 @@
+package 文件系统;
+
+class Inode {
+      int inodeNum;// 索引号
+      int inodeFlag;  // 是否被使用
+      DataBlock datablock;  //声明数据块子对象
+      
+    /*初始化*/
+    Inode(){
+    	inodeFlag=0;  //初始成未被使用
+    	datablock=null;
+    }
+   void write(String fileContenta){
+	    int x=0;  //偏移参数		
+		datablock=SuperBlock.dataBlockAllocation();
+		datablock.write(fileContenta, x);
+		inodeFlag=1;// 已经被使用
+    }
+   void delete() {
+	   if(datablock!=null)
+		   datablock.delete();  //调用数据块删除
+	   inodeFlag=0;
+	   datablock=null;
+   }
+}
