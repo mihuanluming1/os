5a6118a6a5b51db29703b1663be59c8ae5796509
FengXin518 <75883160+FengXin518@users.noreply.github.com>
2021-01-07 10:25:23 +0800

    Update SuperBlock.java

--- FileManagement/src/SuperBlock.java
+++ FileManagement/src/SuperBlock.java
@@ -2,19 +2,25 @@
 public class SuperBlock {
 	private static DataBlock[] dataBlock = new DataBlock[10];
 	private static Inode[] inode= new Inode[10];
-	private boolean dirty = false;
-	static Inode InodeAllocation() {
+	private boolean dirty = false;//脏位为真时需要写回磁盘；为假时说明内存中文件没有被改变，不需要写回磁盘
+	static Inode InodeAllocation() {//索引分配
 		for(int i=0;i<10;i++)
 			if(inode[i].i_flag==0) {
 				return inode[i];
 			}		
 		return inode[0];
 	}
-	static DataBlock DataBlockAllocation() {//���ݿ����,���ڿյ����ݿ鷵�ظ����ݿ����
+	static DataBlock DataBlockAllocation() {//数据块分配,存在空的数据块返回该数据块对象
 		for(int i=0;i<10;i++)
 			if(dataBlock[i].d_flag==0) {
 				return dataBlock[i];
 			}
 		return dataBlock[0];
+	}
+	boolean getDirty() {//获取脏位
+		return dirty;
+	}
+	void modifyDirty() {//修改脏位
+		dirty = true;
 	}
 }
