889db34f93aa80c9074929226c972c018bb387c7
FengXin518 <75883160+FengXin518@users.noreply.github.com>
2021-01-07 10:12:41 +0800

    Add files via upload

---
+++ FileManagement/src/DataBlock.java
@@ -0,0 +1,11 @@
+
+public class DataBlock {
+	int d_flag;
+	private char[] array=new char [10];
+	DataBlock(){
+		
+	}
+	void SetArray(char[] a) {
+		
+	}
+}
--- FileManagement/src/SuperBlock.java
+++ FileManagement/src/SuperBlock.java
@@ -1,41 +1,20 @@
 
 public class SuperBlock {
-	private static DataBlock[] datablock = new DataBlock[10];
+	private static DataBlock[] dataBlock = new DataBlock[10];
 	private static Inode[] inode= new Inode[10];
 	private boolean dirty = false;
-	static int[] InodeNum = {0,0,0,0,0,0,0,0,0,0}; 
-	static int[] DataBlockNum = {0,0,0,0,0,0,0,0,0,0}; 
-	static int[] GetInodeNum()//获得索引分配状态
-	{ 
-		return InodeNum;
+	static Inode InodeAllocation() {
+		for(int i=0;i<10;i++)
+			if(inode[i].i_flag==0) {
+				return inode[i];
+			}		
+		return inode[0];
 	}
-	static int[]  GetDataBlockNum()//获得数据块分配状态
-	{ 
-		return DataBlockNum;
+	static DataBlock DataBlockAllocation() {//数据块分配,存在空的数据块返回该数据块对象
+		for(int i=0;i<10;i++)
+			if(dataBlock[i].d_flag==0) {
+				return dataBlock[i];
+			}
+		return dataBlock[0];
 	}
-	static boolean InodeAllocation(int inodeNum) {//索引分配,该索引号若未分配，进行分配，若已分配，返回false
-		if(InodeNum[inodeNum]==0)
-		{
-			InodeNum[inodeNum]=1;
-			return true;
-		}
-		else
-			return false;
-	}
-	static boolean DataBlockAllocation(int dataBlockNum,char[] cotent) {//数据块分配,DataBlockNum是分配出去的数据块序号
-		if(DataBlockNum[dataBlockNum]==0)
-		{
-			DataBlockNum[dataBlockNum]=1;
-			dataBlock[dataBlockNum].array=cotent;
-			return true;
-		}
-		else
-			return false;
-	}
-	static void DeleteInode(int InodeNum)//通过索引号删除文件，清除目录中下的文件，由目录提出请求
-	{
-		
-	}
-	
-	
 }
